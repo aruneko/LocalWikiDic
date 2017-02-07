e7a1bb264b89863c70ed518c87e0ac3f03e6bd88
Aruneko <aruneko99@gmail.com>
2017-02-07 15:01:27 +0900

    initial commit

---
+++ createLocalWikiDic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+import pandas as pd
+
+sheets = pd.ExcelFile('./data/regional_words_localWiki.xlsx').parse('Sheet1')
+
+for w, y in zip(sheets['単語'], sheets['読み']):
+    print(f"{w},,,1,名詞,固有名詞,地域,一般,*,*,{w},{y},{y},地域固有")
